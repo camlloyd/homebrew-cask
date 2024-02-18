327d769715c7666d00ffdbdae1c99d57e95f0628
Cameron Lloyd <lloyd@med.unideb.hu>
2024-02-18 19:41:20 +0000

    qupath 0.5.0

---
+++ Casks/q/qupath.rb
@@ -0,0 +1,27 @@
+cask "qupath" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.5.0"
+  sha256 arm:   "e08fa1542298b7c7ef56acfeba10febc7c1c7dd2f3e11544a01723f100f99402",
+         intel: "6d36907995ccfd54ce514ad7a7011f8f7988ce285923fff526d1d19a891cf5ad"
+
+  language "en", default: true do
+    "en_US"
+  end
+
+  url "https://github.com/qupath/qupath/releases/download/v#{version}/QuPath-v#{version}-Mac-#{arch}.pkg",
+      verified: "github.com/qupath/qupath/"
+  name "QuPath"
+  desc "Bioimage analysis & digital pathology"
+  homepage "https://qupath.github.io/"
+
+  livecheck do
+    url :url
+  end
+
+  auto_updates true
+
+  pkg "QuPath-v#{version}-Mac-#{arch}.pkg"
+
+  uninstall pkgutil: "qupath"
+end
