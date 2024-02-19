bf53e7958be16154cdf631bb661822b53591669c
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-02-19 13:22:56 +0000

    Add macOS check

--- Casks/q/qupath.rb
+++ Casks/q/qupath.rb
@@ -1,6 +1,8 @@
 cask "qupath" do
+  unless (/darwin/ =~ RUBY_PLATFORM) != nil
+    raise "This cask can only be installed on macOS"
+  end
   arch arm: "arm64", intel: "x64"
-
   version "0.5.0"
   sha256 arm:   "e08fa1542298b7c7ef56acfeba10febc7c1c7dd2f3e11544a01723f100f99402",
          intel: "6d36907995ccfd54ce514ad7a7011f8f7988ce285923fff526d1d19a891cf5ad"
